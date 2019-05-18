ca279b3791afe7e063354e800af719309df60da8
FlawlessCow <michaeldw@gmail.com>
2019-05-18 14:39:27 -0700

    Better format the dump of recoup time

--- home/scripts/character/hacknet/server_autobuy-ns2.js
+++ home/scripts/character/hacknet/server_autobuy-ns2.js
@@ -299,11 +299,11 @@ function evaluateHacknetPurchaseOptions(ns, maxNodes, maxLevel, maxRam, maxCores
 	// Do comparisons to see what to buy
 	ns.print("=== Ready to make a decision ===");
 	ns.print("Getting the minimum of: ");
-	ns.print("--- bareNodeRecoupTime:" + bareNodeRecoupTime);
-	ns.print("--- upgradedNodeRecoupTime:" + upgradedNodeRecoupTime);
-	ns.print("--- levelRecoupTime:" + levelRecoupTime);
-	ns.print("--- ramRecoupTime:" + ramRecoupTime);
-	ns.print("--- coresRecoupTime:" + coresRecoupTime);
+	ns.print("--- bareNodeRecoupTime:" + ns.nFormat(bareNodeRecoupTime, "0,0"));
+	ns.print("--- upgradedNodeRecoupTime:" + ns.nFormat(upgradedNodeRecoupTime, "0,0"));
+	ns.print("--- levelRecoupTime:" + ns.nFormat(levelRecoupTime, "0,0"));
+	ns.print("--- ramRecoupTime:" + ns.nFormat(ramRecoupTime, "0,0"));
+	ns.print("--- coresRecoupTime:" + ns.nFormat(coresRecoupTime, "0,0"));
 	var minRecoupTime = Math.min(bareNodeRecoupTime, upgradedNodeRecoupTime, levelRecoupTime, ramRecoupTime, coresRecoupTime);
 	
 	if (bareNodeRecoupTime === minRecoupTime)
