0db3cd50b7551c45083c357eebc19adeac03424b
FlawlessCow <michaeldw@gmail.com>
2019-05-19 15:49:11 -0700

    Getting root access now handled by the install script, this saves on RAM usage of the run.script. Also updating getRootAccess to only try to get access if it doesn't have it already.

--- home/scripts/functions/getRootAccess-ns2.js
+++ home/scripts/functions/getRootAccess-ns2.js
@@ -1,23 +1,25 @@
 export function getRootAccess (ns, target)
 {
-    // If we have the various port oepning program, use them to open ports
-    // on the target server
-    if (ns.fileExists("BruteSSH.exe", "home")) {
-        ns.brutessh(target);
+	if (hasRootAccess(target) === false) {
+        // If we have the various port oepning program, use them to open ports
+        // on the target server
+        if (ns.fileExists("BruteSSH.exe", "home")) {
+            ns.brutessh(target);
+        }
+        if (ns.fileExists("FTPCrack.exe", "home")) {
+            ns.ftpcrack(target);
+        }
+        if (ns.fileExists("relaySMTP.exe", "home")) {
+            ns.relaysmtp(target);
+        }
+        if (ns.fileExists("HTTPWorm.exe", "home")) {
+            ns.httpworm(target);
+        }
+        if (ns.fileExists("SQLInject.exe", "home")) {
+            ns.sqlinject(target);
+        }
+        
+        // Get root access to target server
+        ns.nuke(target);
     }
-    if (ns.fileExists("FTPCrack.exe", "home")) {
-        ns.ftpcrack(target);
-    }
-    if (ns.fileExists("relaySMTP.exe", "home")) {
-        ns.relaysmtp(target);
-    }
-    if (ns.fileExists("HTTPWorm.exe", "home")) {
-        ns.httpworm(target);
-    }
-    if (ns.fileExists("SQLInject.exe", "home")) {
-        ns.sqlinject(target);
-    }
-    
-    // Get root access to target server
-    ns.nuke(target);
 }
