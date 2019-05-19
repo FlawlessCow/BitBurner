8d3e7805237834edb28054075594a82ab1b2ee4d
FlawlessCow <michaeldw@gmail.com>
2019-05-19 13:00:56 -0700

    Updating recoup time dump to show time in minutes as well

--- home/scripts/character/hacknet/server_autobuy-ns2.js
+++ home/scripts/character/hacknet/server_autobuy-ns2.js
@@ -300,11 +300,11 @@ function evaluateHacknetPurchaseOptions(ns, maxNodes, maxLevel, maxRam, maxCores
 	// Do comparisons to see what to buy
 	ns.print("=== Ready to make a decision ===");
 	ns.print("Getting the minimum of: ");
-	ns.print("--- bareNodeRecoupTime:" + ns.nFormat(bareNodeRecoupTime, "0,0"));
-	ns.print("--- upgradedNodeRecoupTime:" + ns.nFormat(upgradedNodeRecoupTime, "0,0"));
-	ns.print("--- levelRecoupTime:" + ns.nFormat(levelRecoupTime, "0,0"));
-	ns.print("--- ramRecoupTime:" + ns.nFormat(ramRecoupTime, "0,0"));
-	ns.print("--- coresRecoupTime:" + ns.nFormat(coresRecoupTime, "0,0"));
+	ns.print("--- bareNodeRecoupTime:" + ns.nFormat(bareNodeRecoupTime, "0,0") + " seconds (" + ns.nFormat(bareNodeRecoupTime/60, "0,0") + " minutes)");
+	ns.print("--- upgradedNodeRecoupTime:" + ns.nFormat(upgradedNodeRecoupTime, "0,0") + " seconds (" + ns.nFormat(upgradedNodeRecoupTime/60, "0,0") + " minutes)");
+	ns.print("--- levelRecoupTime:" + ns.nFormat(levelRecoupTime, "0,0") + " seconds (" + ns.nFormat(levelRecoupTime/60, "0,0") + " minutes)");
+	ns.print("--- ramRecoupTime:" + ns.nFormat(ramRecoupTime, "0,0") + " seconds (" + ns.nFormat(ramRecoupTime/60, "0,0") + " minutes)");
+	ns.print("--- coresRecoupTime:" + ns.nFormat(coresRecoupTime, "0,0") + " seconds (" + ns.nFormat(coresRecoupTime/60, "0,0") + " minutes)");
 	var minRecoupTime = Math.min(bareNodeRecoupTime, upgradedNodeRecoupTime, levelRecoupTime, ramRecoupTime, coresRecoupTime);
 	
 	if (bareNodeRecoupTime === minRecoupTime && bareNodeRecoupTime < recoupTimeCap)
